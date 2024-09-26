4d847b49a861ce5ecde4df2218d4157003a1b55e
icedwater <icedwater@gmail.com>
2024-09-26 09:19:17 +0800

    Updated vis_utils with notes and fixed whitespace.

--- visualize/vis_utils.py
+++ visualize/vis_utils.py
@@ -24,15 +24,21 @@ class npy2obj:
         self.num_frames = self.motions['motion'][self.absl_idx].shape[-1]
         self.j2s = joints2smpl(num_frames=self.num_frames, device_id=device, cuda=cuda)
 
+        ## if there are just 3 features (xyz?) run SMPLify which updates the motion to a 6-feature model
+        ## TODO: figure out what the 3 features used here are
         if self.nfeats == 3:
-            print(f'Running SMPLify For sample [{sample_idx}], repetition [{rep_idx}], it may take a few minutes.')
+            print(f'Running SMPLify for sample [{sample_idx}], repetition [{rep_idx}], it may take a few minutes.')
+            print("NOTE: This converts rotations to a 6D representation and adds 2 'joints', e.g. 22x3 -> 24x6")
+            print("NOTE: This then adds root node locations, e.g. 24x6 + 1x6 -> 25x6")
+
             motion_tensor, opt_dict = self.j2s.joint2smpl(self.motions['motion'][0][self.absl_idx].transpose(2, 0, 1))  # [nframes, njoints, 3]
-            self.motions['motion'] = motion_tensor.cpu().numpy()        # how does this change to (1, 25, 6, 196) from (1, 22, 3, 196)??
+            self.motions['motion'] = motion_tensor.cpu().numpy()        # how does this change to (1, 25, 6, 196) from (1, 22, 3, 196)?? See NOTEs above.
         elif self.nfeats == 6:
             self.motions['motion'] = self.motions['motion'][[self.absl_idx]]
         self.bs, self.njoints, self.nfeats, self.nframes = self.motions['motion'].shape
         self.real_num_frames = self.motions['lengths'][0][self.absl_idx]
 
+        print("NOTE: Finally, the 6D motion is converted back to xyz (3 dimensions) here.")
         self.vertices = self.rot2xyz(torch.tensor(self.motions['motion']), mask=None,
                                      pose_rep='rot6d', translation=True, glob=True,
                                      jointstype='vertices',
@@ -46,7 +52,7 @@ class npy2obj:
     def get_trimesh(self, sample_i, frame_i):
         return Trimesh(vertices=self.get_vertices(sample_i, frame_i),
                        faces=self.faces)
-    
+
     def get_traj_sphere(self, mesh):
         root_posi = np.copy(mesh.vertices).mean(0) # (6000, 3)
         root_posi[1]  = self.vertices.numpy().min(axis=(0, 1, 3))[1] + 0.1      # why use min() from 3 axes?
@@ -63,7 +69,7 @@ class npy2obj:
         with open(ground_save_path, 'w') as fw:
             ground_sph_mesh.export(fw, 'obj')
         return save_path
-    
+
     def save_npy(self, save_path):
         data_dict = {
             'motion': self.motions['motion'][0, :, :, :self.real_num_frames],
